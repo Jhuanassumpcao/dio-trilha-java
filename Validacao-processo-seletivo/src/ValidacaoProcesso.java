d5eaa2179228d4ccaacf35ef6552d04d266cd0ff
jhuanassumpcao <jhuanassumpcao@gmail.com>
2022-10-15 01:35:04 -0300

    Update

--- Validacao-processo-seletivo/src/ValidacaoProcesso.java
+++ Validacao-processo-seletivo/src/ValidacaoProcesso.java
@@ -1,9 +1,39 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ValidacaoProcesso {
     public static void main(String[] args) throws Exception {
-        analisarCandidato(1900.0);
-		analisarCandidato(2200.0);
-		analisarCandidato(2000.0);
+        selecaoCandidados();
     }
+
+	static void selecaoCandidados() {
+		// Array com a lista de candidatos
+
+		String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+
+		int cadidatosSelecionados = 0;
+		int candidatoAtual = 0;
+		double salarioBase = 2000.0;
+		while(cadidatosSelecionados < 5 ){
+			String candidato = candidatos[candidatoAtual];
+			double salarioPretendido = valorPretendido();
+			System.out.println("O cadidato " + candidato + " Solicitou " + salarioPretendido);
+			if(salarioBase >= salarioPretendido) {
+				System.out.println("O cadidato " + candidato + " foi selecionado ");
+
+				cadidatosSelecionados++;
+			}
+			candidatoAtual++;
+		}
+
+	}
+
+// Método que simula o valor pretendido
+
+
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
+	}
+
 	static void analisarCandidato(double salarioPretendido) {
 		double salarioBase = 2000.0;
 		if(salarioBase > salarioPretendido) {
